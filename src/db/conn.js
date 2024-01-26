1a2cb8a6a45b2a8e605b22dbb79a04056f249286
P00NAM <singhyashika1104@gmail.com>
2024-01-26 16:30:51 +0530

    database url

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/registration-data", 
+mongoose.connect("mongodb+srv://singhyashika1104:"+encodeURIComponent('Poonam#4321')+"@cluster0.36vy8o7.mongodb.net/?retryWrites=true&w=majority", 
 { autoIndex:false, useNewUrlParser:true, useUnifiedTopology:true
 }).then(() => {console.log("connection successfully");
 }).catch((err) => {
