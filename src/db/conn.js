058085dfe7d2ab8b199941a7c7126bedb0ceb417
P00NAM <singhyashika1104@gmail.com>
2024-01-26 16:47:12 +0530

    hdhhfhf

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://singhyashika1104:"+encodeURIComponent('Poonam%234321')+"@cluster0.36vy8o7.mongodb.net/?retryWrites=true&w=majority", 
+mongoose.connect("mongodb+srv://singhyashika1104:Poonam123@cluster0.36vy8o7.mongodb.net/?retryWrites=true&w=majority", 
 { autoIndex:false, useNewUrlParser:true, useUnifiedTopology:true
 }).then(() => {console.log("connection successfully");
 }).catch((err) => {
